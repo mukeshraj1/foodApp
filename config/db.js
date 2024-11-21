6c17a11a000c8ce0bae27e32fe7bf08d7b35edb2
mukeshraj1 <126648673+mukeshraj1@users.noreply.github.com>
2024-11-21 10:43:02 +0530

    final change

--- config/db.js
+++ config/db.js
@@ -1,16 +1,3 @@
-// const mongoose=require('mongoose')
-// const colors=require('colors')
-
-// //function mongodb database coonection
-// const connectDB=async(req,res)=>{
-//     try{
-//         await mongoose.connect(process.env.MONGO_URL)
-//         console.log(`Connected to database ${mongoose.connection.host}`.bgWhite)
-//     }catch(error){
-//         console.log('DB Error',error);
-//     }
-// }
-// module.exports=connectDB
 
 const mongoose = require('mongoose');
 const colors = require('colors');
@@ -21,7 +8,7 @@ const connectDB = async (req,res) => {
         await mongoose.connect(process.env.MONGO_URL);
         console.log(`Connected to database ${mongoose.connection.host}`.red.bgWhite);
     } catch (error) {
-        console.log('DB Error:', error.message.bgRed);  // Use error.message for clarity
+        console.log('DB Error:', error.message.bgRed);  
     }
 }
 
